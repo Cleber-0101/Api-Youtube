2f55850c974e77da9f862233b28b2206ce734ac1
Cleber-0101 <cleberbatista99@hotmail.com>
2024-10-17 19:44:19 -0300

    Criado o metodo de listagem dos videos

--- youtubeAPI/Controllers/HomeController.cs
+++ youtubeAPI/Controllers/HomeController.cs
@@ -9,18 +9,44 @@ namespace youtubeAPI.Controllers
     public class HomeController : Controller
     {
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var videos = await BuscarVideoDoCanalCasemiro();
+            return View(videos);
         }
 
         private async Task<List<DetalhesDosVideos>> BuscarVideoDoCanalCasemiro()
         {
             var youtubeServices = new YouTubeService(new BaseClientService.Initializer
             {
-                ApiKey = "AIzaSyB_fLZcJiXvruTZQL2Ic4u9uYgbUkRuOH4",
+                ApiKey = "testeeeee",
                 ApplicationName = "YoutubeApiVideo"
             });
+
+            var searchRequest = youtubeServices.Search.List("snippet");
+            searchRequest.ChannelId = "@CortesdoCasimitoOFICIAL";
+            //organizar pela ordem de Data 
+            searchRequest.Order  = SearchResource.ListRequest.OrderEnum.Date;
+            searchRequest.MaxResults = 20;
+
+            //executa a solicitação , usei o await porque pode ser que demore 
+            var searchResponse = await searchRequest.ExecuteAsync();
+
+            List<DetalhesDosVideos> videoList = searchResponse.Items.Select(item =>
+                                      new DetalhesDosVideos
+                                      {
+                                          Titulo = item.Snippet.Title,
+                                          Descricao = item.Snippet.Description,
+                                          tumb = item.Snippet.Thumbnails.Medium.Url,
+                                          Link = $"https://www.youtube.com/watch?v={item.Id.VideoId}",
+                                          publicacao = item.Snippet.PublishedAt
+                                      }
+            ).OrderByDescending(video
+             => video.publicacao)
+             .ToList();
+
+            return videoList;
         }
 
     }
--- youtubeAPI/Models/DetalhesDosVideos.cs
+++ youtubeAPI/Models/DetalhesDosVideos.cs
@@ -3,7 +3,7 @@
     public class DetalhesDosVideos
     {
         //pode ser que os dados venham nulos
-        public string? Title { get; set; }
+        public string? Titulo { get; set; }
         public string? Descricao { get; set; }
         public string?  Link { get; set; }
         public string? tumb { get; set; }
